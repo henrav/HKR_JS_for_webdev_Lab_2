75ce572a208b75478497e8883571c7996a66976b
henrav <henrik@ravnborg.se>
2025-12-18 09:06:34 +0100

    added projectsdescritions and pictures

---
+++ JS/projectData.js
@@ -0,0 +1,57 @@
+
+export const projects = [
+    {
+        id: 1,
+        title: "Portfolio Website",
+        description: "My personal portfolio built with HTML, CSS, and JavaScript",
+        category: "Web Development",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        image: "images/project1.jpg",
+        link: "https://example.com"
+    },
+    {
+        id: 2,
+        title: "Aiwebbexam",
+        description: "First website created for school project, very bad, behind login wall so noone can access",
+        category: "Web Development",
+        technologies: ["HTML", "CSS", "JavaScript", "React", "Node.js", "express.js", "SQL"],
+        image: "images/project2.jpg",
+        link: "https://aiwebbexam.se"
+    },
+    {
+        id: 3,
+        title: "Henrikcal",
+        description: "A Unix 'cal'-style CLI calendar written in C. Supports printing a full year (-y) and selecting month (-m), selecting span (-s) and selecting width (-w)",
+        category: "C",
+        technologies: ["C"],
+        image: "images/project3.jpg",
+        link: "https://github.com/henrav/HKR_UnixCal_DT555B"
+    },
+    {
+        id: 4,
+        title: "budget_Junit",
+        description: "A small budget Junit exercise to refresh Java knowledge",
+        category: "Java",
+        technologies: ["Java", "Shell", "Junit"],
+        image: "images/project2.jpg",
+        link: "https://github.com/henrav/budget_Junit"
+    },
+    {
+        id: 5,
+        title: "budgetJavaSQLParserTest",
+        description: "A budget Java project for experimenting with creating a SQL parser. Current project.",
+        category: "Java",
+        technologies: ["Java", "SQL"],
+        image: "images/project5.jpg",
+        link: "https://github.com/henrav/budgetJavaSQLParserTest",
+    },
+    {
+        id: 6,
+        title: "Spel-",
+        description: "Some unfinished C++ game",
+        category: "C++",
+        technologies: ["C++", "SFML"],
+        image: "images/project5.jpg",
+        link: "https://github.com/henrav/Spel-",
+    },
+];
