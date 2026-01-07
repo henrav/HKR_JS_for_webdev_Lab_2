37ebd7a247702769ce9ec8254d6177886dc64e23
henrav <henrik@ravnborg.se>
2026-01-07 14:29:52 +0100

    redthing added to message counter

--- JS/formValidationModule.js
+++ JS/formValidationModule.js
@@ -4,6 +4,7 @@ const validateEmail = (v) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
 
 const validateSubject = (v) => v.length >= 3;
 const validateMessage = (v) => {
+
     increaseCount(v.length)
     return v.length >= 20;
 }
@@ -140,12 +141,19 @@ function resetForm() {
  */
 function increaseCount(v) {
     const charCount = document.getElementById('char-count');
-    if (v >= 20){
-        charCount.innerText = "20/20";
+    if (v >= 20 && v < 40){
+        charCount.innerText = `${v === 0 ? 0 : v}/40`;
         charCount.classList.add("char-count-reached");
-    }else{
-        charCount.innerText = `${v === 0 ? 0 : v}/20`;
+        charCount.classList.remove("redthing");
+    }
+    if (v >= 40){
+        charCount.classList.add("redthing");
+        charCount.innerText = `${v === 0 ? 0 : v}/40`;
+    }
+    if (v < 20){
+        charCount.innerText = `${v === 0 ? 0 : v}/40`;
         charCount.classList = "char-count";
+        charCount.classList.add("redthing");
     }
 }
 
